9f1524003245d5149cc5330ee717b93319d2efda
GustavoPazos <gpazos@moventiglobal.com>
2025-01-27 18:52:38 -0500

    logout fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { authService } from './services/auth';
 import Home from './pages/Home';
 import { useNavigate } from 'react-router-dom';
 
-export const App: React.FC = () => {
+const App: React.FC = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();  // Añadido el hook useNavigate
@@ -52,3 +52,4 @@ export const App: React.FC = () => {
     </Router>
   );
 };
+export default App
